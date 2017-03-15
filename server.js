c431e85e919d9ebce12db86b3e45569dadfa39a1
Louis Kraemer <louiskraemer@gmail.com>
2017-03-15 10:36:41 +0100

    Bug fix : changed port

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const PORT = process.env.PORT || 3000;
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -62,8 +64,8 @@ var fruit = undefined;
 // On gère les requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
 app.use("/", express.static(__dirname + "/public"));
 // On lance le serveur en écoutant les connexions arrivant sur le port 3000
-http.listen(3000, function () {
-    console.log('Server is listening on *:3000');
+http.listen(PORT, function () {
+    console.log('Server is listening on ' + PORT);
 });
 io.on('connection', function (socket) {
     console.log(socket.id + ' connected');
